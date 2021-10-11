05f101c07ef6e0e7a6d17d93b28b5ab3718ab1f3
skyCoder <cs933519>
2021-10-11 19:42:21 +0200

    remove migration database history and change api name

--- vue_app/urls.py
+++ vue_app/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url(r'^employee$',views.tweetApi),
-    url(r'^employee/([0-9]+)$',views.tweetApi),
+    url(r'^tweet$',views.tweetApi),
+    url(r'^tweet/([0-9]+)$',views.tweetApi),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
